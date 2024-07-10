722d603bc2f06dfbca4af10a67bb592a5e80ece0
Nicolas Rivas Meneses <nicolasrivasmeneses23@gmail.com>
2024-07-10 00:21:03 -0500

    init commit

---
+++ src/test/java/com/linktic/service/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktic.service.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
