9545331b1ce7e38c082e2c4acdebf88fcdc92828
Nicolas Rivas Meneses <nicolasrivasmeneses23@gmail.com>
2024-07-10 22:41:37 -0500

    controller refactoring

--- src/main/java/com/linktic/service/product/controller/ProductController.java
+++ src/main/java/com/linktic/service/product/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.linktic.service.product.controller;
 import com.linktic.service.product.entity.Product;
 import com.linktic.service.product.service.ProductServiceI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,20 +17,43 @@ public class ProductController {
     private final ProductServiceI productServiceI;
 
     @GetMapping
-    public List<Product> obtenerTodosLosProductos() {
-        return productServiceI.obtenerTodosLosProductos();
+    public ResponseEntity<List<Product>> obtenerTodosLosProductos() {
+        try {
+            List<Product> products = productServiceI.obtenerTodosLosProductos();
+            if (products.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
     @PostMapping
-    public Product guardarProducto(@RequestBody Product product) {
-        return productServiceI.guardarProducto(product);
+    public ResponseEntity<Product> guardarProducto(@RequestBody Product product) {
+        try {
+            Product savedProduct = productServiceI.guardarProducto(product);
+            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
     @GetMapping("/{id}")
-    public Product obtenerProductoPorId(@PathVariable String id) {
-        return productServiceI.obtenerProductoPorId(id);
+    public ResponseEntity<Product> obtenerProductoPorId(@PathVariable String id) {
+        Product product = productServiceI.obtenerProductoPorId(id);
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void eliminarProducto(@PathVariable String id) {
-        productServiceI.eliminarProducto(id);
+    public ResponseEntity<Void> eliminarProducto(@PathVariable String id) {
+        try {
+            productServiceI.eliminarProducto(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
